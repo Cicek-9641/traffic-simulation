86e8ccad0d6531afb268df2ece6f50864605981a
xtza478 <154631279+xtza478@users.noreply.github.com>
2023-12-23 23:48:37 +0300

    Add files via upload

--- src/trafficsimulation/Road.java
+++ src/trafficsimulation/Road.java
@@ -1,195 +1,181 @@
-package trafficsimulation;
+package TrafikSimulasyon;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
 
 public class Road extends JPanel {
-
-    final int LANE_HEIGHT = 150;
-    final int ROAD_WIDTH = 1200;
-    ArrayList<Vehicle> cars = new ArrayList<>();
-    ArrayList<GasStation> gasStations = new ArrayList<>();
-
-    public Road() {
-        super();
-        // Örnek olarak bir benzin istasyonu ekleme
-        GasStation gasStation = new GasStation(300, LANE_HEIGHT * 3, this);
-        GasStation gasStation2 = new GasStation(800, LANE_HEIGHT * 2, this);
-        gasStations.add(gasStation);
-        gasStations.add(gasStation2);
-
-    }
-
-    public void addCar(Vehicle v) {
-        cars.add(v);
-    }
-
-    public void addGasStation(GasStation gasStation) {
-        gasStations.add(gasStation);
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(Color.GRAY);
-        g.fillRect(0, 0, getWidth(), getHeight());
-        g.setColor(Color.WHITE);
-
-        // Yolların çizimi
-        for (int a = LANE_HEIGHT; a < LANE_HEIGHT * 4; a = a + LANE_HEIGHT) {
-            for (int b = 0; b < getWidth(); b = b + 40) {
-                g.fillRect(b, a, 30, 5);
-            }
-        }
-
-        // Araçların çizimi
-        for (int a = 0; a < cars.size(); a++) {
-            cars.get(a).paintMe(g);
-        }
-
-        // Benzin istasyonlarının çizimi
-        for (int i = 0; i < gasStations.size(); i++) {
-            gasStations.get(i).paintMe(g);
-        }
-
-    }
-
-    public void step() {
-//        for (int a = 0; a < cars.size(); a++) {
-//            Vehicle v = cars.get(a);
-//            if (collision(v.getX() + v.getSpeed(), v.getY(), v) == false) {
-//                v.setX(v.getX() + v.getSpeed());
-//                if (v.getX() > ROAD_WIDTH) {
-//                    if (collision(0, v.getY(), v) == false) {
-//                        v.setX(0);
-//                    }
-//
-//                }
-//            } else {// car ahead
-//                if ((v.getY() > 40)
-//                        && (collision(v.getX(), v.getY() - LANE_HEIGHT, v) == false)) {
-//                    v.setY(v.getY() - LANE_HEIGHT);
-//
-//                } 
-//                else if((v.getY() < 40 + 3 * LANE_HEIGHT)
-//                            && (collision(v.getX(), v.getY() + LANE_HEIGHT, v) == false)){
-//                    v.setY(v.getY() + LANE_HEIGHT);
-//                }
-//
-//            }
-//        }
-
-//        for (int a = 0; a < cars.size(); a++) {
-//            Vehicle v = cars.get(a);
-//            v.setX(v.getX() + v.getSpeed());
-//            v.decreaseFuel();  // Her adımda benzin miktarını azalt
-//            if (v.getFuelLevel() <= 33.3) {  // Benzin miktarı 33.3% veya daha azsa
-//                goToNearestGasStation(v);
-//            }
-//            if (v.getX() > ROAD_WIDTH) {
-//                v.setX(0);
-//            }
-//        }
-
-////////////////
-
-        for (int a = 0; a < cars.size(); a++) {
-            Vehicle v = cars.get(a);
-
-            if (collision(v.getX() + v.getSpeed(), v.getY(), v) == false) {
-                v.setX(v.getX() + v.getSpeed());
-
-                // Check if the vehicle moved beyond the road width
-                if (v.getX() > ROAD_WIDTH) {
-                    if (collision(0, v.getY(), v) == false) {
-                        v.setX(0);
-                    }
-                }
-
-            } else { // Collision with another vehicle ahead
-                if ((v.getY() > 40) && (collision(v.getX(), v.getY() - LANE_HEIGHT, v) == false)) {
-                    v.setY(v.getY() - LANE_HEIGHT);
-
-                } else if ((v.getY() < 40 + 3 * LANE_HEIGHT)
-                        && (collision(v.getX(), v.getY() + LANE_HEIGHT, v) == false)) {
-                    v.setY(v.getY() + LANE_HEIGHT);
-                }
-            }
-
-            v.decreaseFuel(); // Decrease fuel at each step
-            if (v.getFuelLevel() <= 33.3) {
-                goToNearestGasStation(v);
-            }
-
-            // Check if the vehicle moved beyond the road width after collision handling
-            if (v.getX() > ROAD_WIDTH) {
-                v.setX(0);
-            }
-        }
-
-  
-
-
-
-    }
-
-    public boolean collision(int x, int y, Vehicle v) {
-        for (int a = 0; a < cars.size(); a++) {
-            Vehicle u = cars.get(a);
-            if (y == u.getY()) {
-                if (u.equals(v) == false) {
-                    if (x < u.getX() + u.getWidth()
-                            && x + v.getWidth() > u.getX()) {
-                        return true;
-
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    // Yeni bir metod ekleyerek en yakın benzin istasyonuna gitme işlemini gerçekleştir
-    private void goToNearestGasStation(Vehicle v) {
-        GasStation nearestStation = findNearestGasStation(v);
-        if (nearestStation != null) {
-            synchronized (nearestStation) {
-                v.setX(nearestStation.x);
-                v.setY(nearestStation.y);
-                try {
-                    Thread.sleep(3000);  // Bekleme süresini ihtiyacınıza göre ayarlayabilirsiniz
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                v.refillFuel();
-            }
-        }
-    }
+	
  
+	private ImageIcon redIcon = new ImageIcon("red.png");
+	private ImageIcon greenIcon = new ImageIcon("green.png");
+	private ImageIcon surukleIcon = new ImageIcon("suruklered.png");
+	ArrayList<DragDropLight> dragdrops = new ArrayList<DragDropLight>();
+	ArrayList<DragDropLevha> dragdropslevha = new ArrayList<DragDropLevha>();
+	ArrayList<DragDropRealTimeLight> dragdropsreallight = new ArrayList<DragDropRealTimeLight>();
 
+	private JButton surukleIconn = new JButton(surukleIcon);
 
+	private JButton trafficLightButton = new JButton(redIcon);
+ 
+	final int LANE_HEIGHT = 120;
+	final int ROAD_WIDTH = 800;
+	ArrayList<Vehicle> cars = new ArrayList<Vehicle>();
+	int carCount = 0;
+	  public List<Vehicle> getCars() {
+	        return cars;  
+	    }
+	private boolean isRedLight = true;
 
+	public void saveCarCountToFile() {
+		try {
+			BufferedWriter writer = new BufferedWriter(
+					new FileWriter("trafikBilgi.txt", true));
 
+			LocalDateTime now = LocalDateTime.now();
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+			String formattedDateTime = now.format(formatter);
 
-    // Araç için en yakın benzin istasyonunu bulma
-    private GasStation findNearestGasStation(Vehicle v) {
-        double minDistance = Double.MAX_VALUE;
-        GasStation nearestStation = null;
-        for (GasStation station : gasStations) {
-            double distance = Math.sqrt(Math.pow(v.getX() - station.x, 2) + Math.pow(v.getY() - station.y, 2));
-            if (distance < minDistance) {
-                minDistance = distance;
-                nearestStation = station;
-            }
-        }
-        return nearestStation;
+			writer.write(formattedDateTime + " - " + carCount);
+			writer.newLine();
+			writer.close();
+			System.out.println("Trafikteki toplam araÃ§ sayÄ±sÄ± trafikBilgi.txt dosyasÄ±na kaydediliyor.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	public Road() {
+
+		super();
+		 DragDropLight dragdrop = new DragDropLight(300, LANE_HEIGHT * 3, this);
+		 dragdrops.add(dragdrop);
+		 
+		 DragDropLevha dragdroplevha = new DragDropLevha(400, LANE_HEIGHT * 3, this);
+		 dragdropslevha.add(dragdroplevha);
+		 
+		 DragDropRealTimeLight dragdropsreal  = new DragDropRealTimeLight(500, LANE_HEIGHT * 3, this);
+		 dragdropsreallight.add(dragdropsreal);
+		  
+		trafficLightButton.setBackground(Color.BLACK);
+	//	add(surukleIconn);
+		add(trafficLightButton);
+ 		Timer timer = new Timer(5000, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isRedLight) {
+					trafficLightButton.setIcon(greenIcon);
+				} else {
+					trafficLightButton.setIcon(redIcon);
+				}
+
+				isRedLight = !isRedLight;
+			}
+		});
+
+		timer.start();
+	 
+	}
+	 
+	
+	public void addCar(Vehicle v) {
+		cars.add(v);
+	}
+	
+	public void addDragDropLight(DragDropLight dragdropLight) {
+	    dragdrops.add(dragdropLight);
     }
-    
-    
-     public void clearCars() {
-        cars.clear();
-    }
-    
+	
+     public void addDragDropLevha(DragDropLevha dragdropLevha) {
+		dragdropslevha.add(dragdropLevha);
+	 }
+     
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.GRAY);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(Color.WHITE);
+		for (int a = LANE_HEIGHT; a < 600; a = a + LANE_HEIGHT) {
+			for (int b = 0; b < getWidth(); b = b + 40) {
+				g.fillRect(b, a, 30, 5);
+			}
+		}
+		for (int a = 0; a < cars.size(); a++) {
+			cars.get(a).paintMe(g);
+		}
+		  
+		   for (int i = 0; i < dragdrops.size(); i++) {
+			   dragdrops.get(i).paintMe(g);
+	       }
+		   for (int i = 0; i < dragdropslevha.size(); i++) {
+			   dragdropslevha.get(i).paintMe(g);
+	       }
+		   
+		   for (int i = 0; i < dragdropsreallight.size(); i++) {
+			   dragdropsreallight.get(i).paintMe(g);
+	       }
+	}
+ 	
+   
+	public void step() {
+		for (int a = 0; a < cars.size(); a++) {
+			Vehicle v = cars.get(a);
+			if (!isRedLight && collision(v.getX() + v.getSpeed(), v.getY(), v) == false) {
+				v.setX(v.getX() + v.getSpeed());
+				if (v.getX() > ROAD_WIDTH) {
+					if (collision(0, v.getY(), v) == false) {
+						v.setX(0);
+						carCount++;
+						saveCarCountToFile();
+					}
+				}
+			} else {
+				if ((v.getY() > 40) && (collision(v.getX(), v.getY() - LANE_HEIGHT, v) == false)) {
+					v.setY(v.getY() - LANE_HEIGHT);
+
+				} else if ((v.getY() > 40 + 3 * LANE_HEIGHT)
+						&& (collision(v.getX(), v.getY() - LANE_HEIGHT, v) == false)) {
+				}
+			}
+		}
+	}
+
+	public boolean collision(int x, int y, Vehicle v) {
+		for (int a = 0; a < cars.size(); a++) {
+			Vehicle u = cars.get(a);
+			if (y == u.getY()) {
+				if (u.equals(v) == false) {
+					if (x < u.getX() + u.getWidth() && x + v.getWidth() > u.getX()) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	public int getCarCount() {
+		return carCount;
+	}
+
+	public void resetCarCount() {
+		carCount = 0;
+	}
+
+
 }
