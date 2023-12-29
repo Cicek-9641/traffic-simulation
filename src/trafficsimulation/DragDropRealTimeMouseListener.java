42dc965b09eea1421ea55b16f8a891526159f90d
xtza478 <154631279+xtza478@users.noreply.github.com>
2023-12-29 17:33:18 +0300

    Add files via upload

--- src/trafficsimulation/DragDropRealTimeMouseListener.java
+++ src/trafficsimulation/DragDropRealTimeMouseListener.java
@@ -3,8 +3,11 @@ package trafficsimulation;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.swing.ImageIcon;
 
 public class DragDropRealTimeMouseListener extends MouseAdapter {
     
@@ -22,48 +25,65 @@ public class DragDropRealTimeMouseListener extends MouseAdapter {
 
         int mouseX = e.getX();
         int mouseY = e.getY();
-
+  
         if (mouseX >= reallight.x && mouseX <= reallight.x + reallight.myImage.getWidth(null) &&
             mouseY >= reallight.y && mouseY <= reallight.y + reallight.myImage.getHeight(null)) {
         	reallight.isDragging = true;
-        }
-    }
+         } 
+    } 
  
-
+ 
 	@Override
-	public void mouseReleased(MouseEvent e) {
-		 int mouseY = e.getY();
- 
-		reallight.isDragging = false;
-  	   // int slowDownAmount = -5;  
-		if (mouseY < 350) {
-	    for (Vehicle car : road.getCars()) {
-	        if (car instanceof Vehicle) {
-	            ((Vehicle) car).speed=0;    
+	public void mouseClicked(MouseEvent e) {
+	    int mouseX = e.getX();
+	    int mouseY = e.getY();
+
+	    if (mouseX >= reallight.x && mouseX <= reallight.x + reallight.myImage.getWidth(null)
+	            && mouseY >= reallight.y && mouseY <= reallight.y + reallight.myImage.getHeight(null)) {
+
+	        if (!reallight.isGreen) {
+	            ImageIcon newImageIcon = new ImageIcon("green.png");
+	            try {
+	                reallight.setImage(newImageIcon);
+	                for (Vehicle car : road.getCars()) {
+	                    if (car instanceof Vehicle) {
+	                        ((Vehicle) car).speed = 30;  
+	                        System.out.println("DEVAM ET");
+	                    }
+	                }
+	                road.repaint();
+	                reallight.isGreen = true;  
+	            } catch (IOException e1) {
+	                e1.printStackTrace();
+	            }
+	        } else {
+	            ImageIcon newImageIcon = new ImageIcon("red.png");
+	            
+	                reallight.setImagee(newImageIcon);
+	                for (Vehicle car : road.getCars()) {
+	                    if (car instanceof Vehicle) {
+	                        ((Vehicle) car).speed = 0;  
+	                        System.out.println("DUR");
+	                    }
+	                }
+	                road.repaint();
+	                reallight.isGreen = false;  
+	        
 	        }
 	    }
- 	    road.repaint();
 	}
-		else {
-			  for (Vehicle car : road.getCars()) {
-			        if (car instanceof Vehicle) {
-			            ((Vehicle) car).speed=25;    
-			        }
-			    }
-		 	    road.repaint();
-			
-		}
-	}
-	
  
- 
+ 	
     @Override
     public void mouseDragged(MouseEvent e) {
-     
+    	
         if (reallight.isDragging) {
         	reallight.x = e.getX();
         	reallight.y = e.getY();
             road.repaint();  
         }
     }
+    
+  
+    
 }
