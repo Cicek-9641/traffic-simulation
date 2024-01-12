c9a3f8cc996b29aadd7b839670061232b8cbcf59
Ennur478 <154631279+Ennur478@users.noreply.github.com>
2024-01-12 03:01:11 +0300

    Update Otomobil.java

--- src/trafficsimulation/Otomobil.java
+++ src/trafficsimulation/Otomobil.java
@@ -13,9 +13,13 @@ public class Otomobil extends Vehicle {
     Image myImage;
 
     public Otomobil(int newx, int newy, Road road,int speed) {
+
         super(newx, newy, road,speed);
+        setType("Otomobil");
+
         width = 40;
-        height = 20;       
+        height = 20;
+      //  speed = 12;     
         fuelLevel = 100.0;
 
         try {
@@ -43,5 +47,17 @@ public class Otomobil extends Vehicle {
          g.setColor(Color.BLACK);
          g.drawRect(x, y + height, width, 5);
     }
+
+//    public void paintMe(Graphics g) {
+//        g.drawImage(myImage, x, y, null);
+//
+//        int barWidth = (int) (width * (fuelLevel / 100));  
+//        g.setColor(Color.YELLOW);  
+//        g.fillRect(x, y + height, barWidth, 5); 
+//
+//        
+//        g.setColor(Color.BLACK);
+//        g.drawRect(x, y + height, width, 5);
+//    }
 
 }
