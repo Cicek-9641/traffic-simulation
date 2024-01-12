51f359a276050f490a4df8080d8a3a94e7ee0981
Ennur478 <154631279+Ennur478@users.noreply.github.com>
2024-01-12 03:00:41 +0300

    Update Vehicle.java

--- src/trafficsimulation/Vehicle.java
+++ src/trafficsimulation/Vehicle.java
@@ -5,32 +5,37 @@ import java.awt.Image;
 import java.awt.Rectangle;
 
 public class Vehicle {
-
-    private static int nextPlakaNumber = 1;
-    public String plaka;
+	  private static int nextPlakaNumber = 1; 
+	    public String plaka;
     int x;
     int y;
     int width = 0;
     int height = 0;
     int speed = 0;
-    double fuelLevel = 0;
+    double fuelLevel = 0;  
+    String type;
+    
+    public String getType() {
+		return type;
+	}
 
-    private int cezaPuani = 0;
-    private boolean allowedToPass = true;
-
-    public String getPlaka() {
+	public void setType(String type) {
+		this.type = type;
+	}
+	private int cezaPuani = 0; 
+    private boolean allowedToPass = true; 
+     public String getPlaka() {
         return plaka;
     }
 
     public void setPlaka(String yeniPlaka) {
         this.plaka = yeniPlaka;
     }
-
     public void showPlaka() {
         System.out.println("Araç Plakası: " + plaka);
     }
     Image fuelGaugeImage;
-
+    
     private int initialSpeed;
 
     Road road;
@@ -39,17 +44,17 @@ public class Vehicle {
         return new Rectangle(x, y, width, height);
     }
 
-    public Vehicle(int newx, int newy, Road road, int speed) {
+    public Vehicle(int newx, int newy, Road road,int speed) {
         x = newx;
         y = newy;
         this.road = road;
         this.initialSpeed = speed;
-        this.speed = speed;
+        this.speed = speed; 
         this.plaka = "SIM-PR" + nextPlakaNumber;
         nextPlakaNumber++;
 
     }
-
+    
     public int getInitialSpeed() {
         return initialSpeed;
     }
@@ -61,10 +66,10 @@ public class Vehicle {
     }
 
     public void goToGasStationAndWait(Road road) {
-        road.addToQueue(this);
+        road.addToQueue(this); 
         synchronized (this) {
             try {
-                wait();
+                wait(); 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -72,10 +77,9 @@ public class Vehicle {
     }
 
     public void paintMe(Graphics g) {
-
-        g.drawString("Plaka: " + plaka, x, y - 10);
+       
+        g.drawString("Plaka: " + plaka, x, y - 10);  
     }
-
     public int getX() {
         return x;
     }
@@ -105,25 +109,34 @@ public class Vehicle {
     }
 
     public void decreaseFuel() {
-        fuelLevel -= 0.5;
+        // Her adımda benzin miktarını azalt
+        fuelLevel -= 0.5;  // Bu değeri ihtiyacınıza göre ayarlayabilirsiniz
     }
 
     public void refillFuel() {
-        fuelLevel = 100.0;
+        // Benzin istasyonuna gidip benzin dolumu yap
+        fuelLevel = 100.0;  // Bu değeri ihtiyacınıza göre ayarlayabilirsiniz
     }
 
-    public void setSpeed(int newSpeed) {
-        speed = newSpeed;
-    }
-
+  public void setSpeed(int newSpeed) {
+    speed = newSpeed;
+}
+  
+  
+  
 // Kırmızı ışıkta geçen araçlara ceza puanı ekleyen metot
     public void addPenaltyPoints(int cezaMiktari) {
         cezaPuani += cezaMiktari;
+
+        // Örneğin: Başka bir sınıfta veya listeye ceza bilgilerini kaydetmek gibi
         System.out.println("Ceza puanı eklendi: " + cezaMiktari + " - Toplam Ceza Puanı: " + cezaPuani);
     }
 
+    // Araçların yeşil ışıkta geçip geçemeyeceğini belirleyen metot
     public void setAllowedToPass(boolean allowed) {
         allowedToPass = allowed;
+
+        // Burada aracın geçiş iznini set etmek için kodu ekleyebilirsiniz
         System.out.println("Geçiş izni güncellendi: " + allowedToPass);
     }
 }
